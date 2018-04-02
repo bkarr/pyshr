346228fa164fe8bcad6ac756da470f6e732bbc32
Bryan Karr <bryankarr@gmail.com>
2018-04-01 22:30:05 -0600

    allow tuples for vector adds

--- pyshr.py
+++ pyshr.py
@@ -1,7 +1,14 @@
-from __about__ import (
-    __author__, __email__, __license__, __summary__, __title__,
-    __uri__, __version__, __libshr_version__
-)
+"""
+    Package is wrapper for the libshr C library implemented using CFFI
+"""
+
+import math
+import signal
+import sys
+
+from __about__ import (__author__, __email__, __libshr_version__, __license__,
+                       __summary__, __title__, __uri__, __version__)
+from _pyshr import ffi, lib
 
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
@@ -9,10 +16,6 @@ __all__ = [
     'SQMode', 'SQEvent', 'SharedQueue'
 ]
 
-from _pyshr import ffi, lib
-import sys
-import math
-import signal
 
 
 class ShareException(Exception):
@@ -107,11 +110,10 @@ class SharedQueue:
         return lib.shr_q_is_valid(name)
 
     def __init__(self, name, mode, size=0):
-        '''
-        constructor
-        name - string name of queue
-        mode - SQMode instance for access
-        size - max size if creating new queue, 0 defaults to system max
+        ''' constructor for queue object
+            name - string name of queue
+            mode - SQMode instance for access
+            size - max size if creating new queue, 0 defaults to system max
         '''
         if not SQMode.is_valid(mode):
             raise ShareException('constructor',
@@ -142,7 +144,8 @@ class SharedQueue:
         self.buff_sz[0] = 0
 
     def destroy(self):
-        ''' destroy queue '''
+        ''' destroy queue 
+        '''
         if self.pq[0] == ffi.NULL:
             raise ShareException('destroy', ShareError.text[lib.SH_ERR_ARG])
         status = lib.shr_q_destroy(self.pq)
@@ -152,7 +155,8 @@ class SharedQueue:
             lib.free(self.buff[0])
 
     def close(self):
-        ''' close queue instance, preserves queue '''
+        ''' close queue instance, preserves queue 
+        '''
         if self.pq[0] == ffi.NULL:
             raise ShareException('close', ShareError.text[lib.SH_ERR_ARG])
         status = lib.shr_q_close(self.pq)
@@ -162,7 +166,8 @@ class SharedQueue:
             lib.free(self.buff[0])
 
     def add(self, data):
-        ''' add data stream to queue '''
+        ''' add data stream to queue 
+        '''
         if not isinstance(data, bytes) and not isinstance(data, str) and \
             not isinstance(data, unicode):
             raise ShareException('add', 'incompatible data type', data)
@@ -172,7 +177,8 @@ class SharedQueue:
             raise ShareException('add', ShareError.text[status])
 
     def add_wait(self, data):
-        ''' add data stream to queue, block if full '''
+        ''' add data stream to queue, block if full 
+        '''
         if not isinstance(data, bytes) and not isinstance(data, str) and \
             not isinstance(data, unicode):
             raise ShareException('add_wait', 'incompatible data type', data)
@@ -210,12 +216,12 @@ class SharedQueue:
                                          ShareError.text[lib.SH_ERR_ARG], items[i])
                 data = items[i][1]
 
-            if d_type == int or (d_type == SHType.INTEGER_T and type(data) == int):
+            if d_type == int or (d_type == SHType.INTEGER_T and isinstance(data, int)):
                 vector[i].type = lib.SH_INTEGER_T
                 vector[i].len = 4
                 buffers.append(ffi.new("int *", data))
                 vector[i].base = buffers[i]
-            elif d_type == long or (d_type == SHType.INTEGER_T and type(data) == long):
+            elif d_type == long or (d_type == SHType.INTEGER_T and isinstance(data, long)):
                 vector[i].type = lib.SH_INTEGER_T
                 vector[i].len = 8
                 buffers.append(ffi.new("long long *", data))
@@ -231,7 +237,7 @@ class SharedQueue:
                 buffers.append(ffi.new('char[]', data))
                 vector[i].base = buffers[i]
             elif d_type == SHType.XML_T:
-                if type(data) == unicode:
+                if isinstance(data, unicode):
                     data = data.encode()
                 vector[i].type = lib.SH_XML_T
                 vector[i].len = len(data)
@@ -264,8 +270,9 @@ class SharedQueue:
                                      data)
 
     def addv(self, items):
-        ''' add list to queue '''
-        if not isinstance(items, list):
+        ''' add list to queue 
+        '''
+        if not isinstance(items, (list, tuple)):
             raise ShareException('add', 'incompatible data type', items)
         vcnt = len(items)
         vector = ffi.new('sq_vec_s[]', vcnt)
@@ -276,8 +283,9 @@ class SharedQueue:
             raise ShareException('add', ShareError.text[status])
 
     def addv_wait(self, items):
-        ''' add arbitrary data to queue, block if full '''
-        if not isinstance(items, list):
+        ''' add arbitrary data to queue, block if full 
+        '''
+        if not isinstance(items, (list, tuple)):
             raise ShareException('add', 'incompatible data type', items)
         vcnt = len(items)
         vector = ffi.new('sq_vec_s[]', vcnt)
@@ -288,12 +296,11 @@ class SharedQueue:
             raise ShareException('add_wait', ShareError.text[status])
 
     def addv_timedwait(self, items, time):
+        ''' add arbitrary data to queue, block for time if full
+            items - list of items to put on queue
+            time - float of time to wait
         '''
-        add arbitrary data to queue, block for time if full
-        data - string or bytes to put on queue
-        time - float of time to wait
-        '''
-        if not isinstance(items, list):
+        if not isinstance(items, (list, tuple)):
             raise ShareException('add', 'incompatible data type', items)
         ts = ffi.new('struct timespec *')
         split = math.modf(time)
@@ -341,7 +348,8 @@ class SharedQueue:
         return result
 
     def remove(self):
-        ''' remove item from queue '''
+        ''' remove item from queue without blocking
+        '''
         item = ffi.new('sq_item_s *')
         item = lib.shr_q_remove(self.pq[0], self.buff, self.buff_sz)
         if item.status == lib.SH_ERR_EMPTY:
@@ -351,7 +359,8 @@ class SharedQueue:
         return self.__to_list(item)
 
     def remove_wait(self):
-        ''' remove item from queue, block if empty '''
+        ''' remove item from queue, block if empty 
+        '''
         item = ffi.new('sq_item_s *')
         item = lib.shr_q_remove_wait(self.pq[0], self.buff, self.buff_sz)
         if item.status == lib.SH_ERR_EMPTY:
@@ -361,10 +370,8 @@ class SharedQueue:
         return self.__to_list(item)
 
     def remove_timedwait(self, time):
-        '''
-        remove item from queue, block for time if empty
-        data - string or bytes to put on queue
-        time - float of time to wait
+        ''' remove item from queue, block for time if empty
+            time - float of time to wait
         '''
         item = ffi.new('sq_item_s *')
         ts = ffi.new('struct timespec *')
@@ -379,7 +386,8 @@ class SharedQueue:
         return self.__to_list(item)
 
     def monitor(self, signo):
-        ''' registers as monitoring process using specified signal '''
+        ''' registers as monitoring process using specified signal 
+        '''
         if signo is None or not isinstance(signo, int):
             raise ShareException('monitor', ShareError.text[lib.SH_ERR_ARG])
         status = lib.shr_q_monitor(self.pq[0], signo)
@@ -387,8 +395,7 @@ class SharedQueue:
             raise ShareException('monitor', ShareError.text[status])
 
     def listen(self, signo):
-        '''
-        registers as listening process for arrivals using specified signal
+        ''' registers as listening process for arrivals using specified signal
         '''
         if signo is None or not isinstance(signo, int):
             raise ShareException('monitor', ShareError.text[lib.SH_ERR_ARG])
@@ -397,8 +404,7 @@ class SharedQueue:
             raise ShareException('monitor', ShareError.text[status])
 
     def call(self, signo):
-        '''
-        registers as called process for blocked removes using specified signal
+        ''' registers as called process for blocked removes using specified signal
         '''
         if signo is None or not isinstance(signo, int):
             raise ShareException('monitor', ShareError.text[lib.SH_ERR_ARG])
@@ -407,23 +413,25 @@ class SharedQueue:
             raise ShareException('monitor', ShareError.text[status])
 
     def event(self):
-        ''' return active event '''
+        ''' return active event 
+        '''
         return int(lib.shr_q_event(self.pq[0]))
 
     def exceeds_idle_time(self, time):
-        ''' tests to see if no item has been added within the specified time '''
+        ''' tests to see if no item has been added within the specified time 
+        '''
         ts = ffi.new('struct timespec *')
         ts.tv_sec = math.trunc(time)
         ts.tv_nsec = int(math.modf(time)[0] * 1000000000)
         return lib.shr_q_exceeds_idle_time(self.pq[0], ts)
 
     def count(self):
-        ''' count of items on queue '''
+        ''' count of items on queue 
+        '''
         return int(lib.shr_q_count(self.pq[0]))
 
     def level(self, depth):
-        '''
-        sets value for queue depth level event generation and for adaptive LIFO
+        ''' sets value for queue depth level event generation and for adaptive LIFO
         '''
         if depth is None or not isinstance(depth, int):
             raise ShareException('level', ShareError.text[lib.SH_ERR_ARG])
@@ -432,8 +440,7 @@ class SharedQueue:
             raise ShareException('level', ShareError.text[status])
 
     def timelimit(self, time):
-        '''
-        sets time limit of item on queue before producing a max time limit event
+        ''' sets time limit of item on queue before producing a max time limit event
         '''
         ts = ffi.new('struct timespec *')
         ts.tv_sec = math.trunc(time)
@@ -443,7 +450,8 @@ class SharedQueue:
             raise ShareException('timelimit', ShareError.text[status])
 
     def clean(self, time):
-        ''' remove items from front of queue that have exceeded timelimit '''
+        ''' remove items from front of queue that have exceeded timelimit 
+        '''
         ts = ffi.new('struct timespec *')
         ts.tv_sec = math.trunc(time)
         ts.tv_nsec = int(math.modf(time)[0] * 1000000000)
@@ -452,7 +460,8 @@ class SharedQueue:
             raise ShareException('clean', ShareError.text[status])
 
     def last_empty(self):
-        ''' returns timestamp of last time queue became non-empty '''
+        ''' returns timestamp of last time queue became non-empty 
+        '''
         ts = ffi.new('struct timespec *')
         ts.tv_sec = 0
         ts.tv_nsec = 0
@@ -462,7 +471,8 @@ class SharedQueue:
         return (ts.tv_sec, ts.tv_nsec)
 
     def discard(self, flag):
-        ''' discard items that exceed expiration time limit '''
+        ''' discard items that exceed expiration time limit 
+        '''
         if flag is None or not isinstance(flag, bool):
             raise ShareException('discard', ShareError.text[lib.SH_ERR_ARG])
         status = lib.shr_q_discard(self.pq[0], flag)
@@ -470,31 +480,38 @@ class SharedQueue:
             raise ShareException('discard', ShareError.text[status])
 
     def will_discard(self):
-        ''' tests to see if queue will discard expired items '''
+        ''' tests to see if queue will discard expired items 
+        '''
         return lib.shr_q_will_discard(self.pq[0])
 
     def limit_lifo(self, flag):
-        ''' treat depth limit as limit for adaptive LIFO behavior '''
+        ''' treat depth limit as limit for adaptive LIFO behavior 
+        '''
         if flag is None or not isinstance(flag, bool):
             raise ShareException('limit_lifo', ShareError.text[lib.SH_ERR_ARG])
+        
         status = lib.shr_q_limit_lifo(self.pq[0], flag)
         if status:
             raise ShareException('limit_lifo', ShareError.text[status])
 
     def will_lifo(self):
-        ''' tests to see if queue will used adaptive LIFO '''
+        ''' tests to see if queue will used adaptive LIFO 
+        '''
         return lib.shr_q_will_lifo(self.pq[0])
 
     def subscribe(self, event):
-        ''' subscribe to see specified event '''
+        ''' subscribe to see specified event 
+        '''
         if not SQEvent.is_valid(event):
             raise ShareException('subscribe', ShareError.text[lib.SH_ERR_ARG])
+        
         status = lib.shr_q_subscribe(self.pq[0], event)
         if status:
             raise ShareException('subscribe', ShareError.text[status])
 
     def unsubscribe(self, event):
-        ''' remove subscription for specified event '''
+        ''' remove subscription for specified event 
+        '''
         if not SQEvent.is_valid(event):
             raise ShareException('unsubscribe', ShareError.text[lib.SH_ERR_ARG])
         status = lib.shr_q_subscribe(self.pq[0], event)
@@ -502,24 +519,28 @@ class SharedQueue:
             raise ShareException('unsubscribe', ShareError.text[status])
 
     def is_subscribed(self, event):
-        ''' returns true if event subscribed, otherwise false '''
+        ''' returns true if event subscribed, otherwise false 
+        '''
         if not SQEvent.is_valid(event):
             raise ShareException('is_subscribed',
                                  ShareError.text[lib.SH_ERR_ARG])
         return lib.shr_q_is_subscribed(self.pq[0], event)
 
     def prod(self):
-        ''' activates at least one blocked caller '''
+        ''' activates at least one blocked caller 
+        '''
         status = lib.shr_q_prod(self.pq[0])
         if status:
             raise ShareException('prod', ShareError.text[status])
 
     def call_count(self):
-        ''' count of blocked remove calls '''
+        ''' count of blocked remove calls 
+        '''
         return int(lib.shr_q_call_count(self.pq[0]))
 
     def target_delay(self, time):
-        ''' sets target delay and activates CoDel algorithm '''
+        ''' sets target delay and activates CoDel algorithm 
+        '''
         ts = ffi.new('struct timespec *')
         ts.tv_sec = math.trunc(time)
         ts.tv_nsec = int(math.modf(time)[0] * 1000000000)
@@ -535,6 +556,7 @@ if __name__ == '__main__':
         print "queue count ", q.count()
         #import pdb; pdb.set_trace()
         q.addv([(SHType.INTEGER_T, 4), (SHType.JSON_T, u'{"test":"value"}'), (SHType.XML_T, '<doc/>')])
+        q.addv(((SHType.INTEGER_T, 4), (SHType.JSON_T, u'{"test":"value"}'), (SHType.XML_T, '<doc/>')))
         q.addv_wait([(SHType.INTEGER_T, 4), (SHType.JSON_T, u'{"test":"value"}'), (SHType.XML_T, '<doc/>')])
         q.addv_timedwait([(SHType.INTEGER_T, 4), (SHType.JSON_T, u'{"test":"value"}'), (SHType.XML_T, '<doc/>')], 0.01)
         q.add_wait("add wait test data")
@@ -547,6 +569,6 @@ if __name__ == '__main__':
         item = q.remove_timedwait(0.01)
         print item
         print "queue count ", q.count()
-        q.close()
+        q.destroy()
     except ShareException as exc:
         print 'queue ops failed:  ', exc
